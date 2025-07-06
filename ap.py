4396fe65902400d742ac93dc107700cee662de36
Dian014 <ekaputradian01@gmail.com>
2025-07-07 01:07:58 +0800

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,4 +1,9 @@
-import streamlit as st import requests import pandas as pd import plotly.express as px import folium from streamlit_folium import st_folium
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+import folium from streamlit_folium
+import st_folium
 
 ------------------ CONFIG ------------------
 
